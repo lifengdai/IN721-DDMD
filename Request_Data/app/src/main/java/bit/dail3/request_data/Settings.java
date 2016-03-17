fc76ca6e0e71a04ef7fc7ed61ad669bc2f5cc2e2
lifengdai <dailifeng@dailifengdeMacBook-Pro.local>
2016-03-17 14:49:52 +1300

    Check
    Check the length of input string name

--- Request_Data/app/src/main/java/bit/dail3/request_data/Settings.java
+++ Request_Data/app/src/main/java/bit/dail3/request_data/Settings.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
@@ -27,10 +28,17 @@ public class Settings extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
-            Intent back = new Intent();
-            back.putExtra(getResources().getString(R.string.extra_string), edttxtName.getText().toString());
-            setResult(Activity.RESULT_OK, back);
-            finish();
+            String name = edttxtName.getText().toString();
+            if(name.length() < 5)
+            {
+                Toast.makeText(Settings.this, getResources().getString(R.string.toast_txt), Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Intent back = new Intent();
+                back.putExtra(getResources().getString(R.string.extra_string), name);
+                setResult(Activity.RESULT_OK, back);
+                finish();
+            }
         }
     }
 }
